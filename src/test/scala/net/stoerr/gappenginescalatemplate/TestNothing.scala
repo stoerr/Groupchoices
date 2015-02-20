a8e8811689e30fab4031078bcfb16e1008fb5055
Hans-Peter Stoerr <http://www.stoerr.net/>
2015-02-20 13:25:38 +0100

    Introduce empty tests and resource files to ensure directories are created

---
+++ src/test/scala/net/stoerr/gappenginescalatemplate/TestNothing.scala
@@ -0,0 +1,16 @@
+package net.stoerr.gappenginescalatemplate
+
+import org.scalatest.FlatSpec
+
+/**
+ * Just an empty test
+ * @author <a href="http://www.stoerr.net/">Hans-Peter Stoerr</a>
+ * @since 20.02.2015
+ */
+class TestNothing extends FlatSpec {
+
+  "Nothing" should "be OK" in {
+    assert(true);
+  }
+
+}
