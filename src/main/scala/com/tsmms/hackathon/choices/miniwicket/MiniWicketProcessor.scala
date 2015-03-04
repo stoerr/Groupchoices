444ed2f50b3f67cfebc641b6e6416f1ca159ae38
Hans-Peter Stoerr <http://www.stoerr.net/>
2015-03-04 13:00:21 +0100

    miniwicket

---
+++ src/main/scala/com/tsmms/hackathon/choices/miniwicket/MiniWicketProcessor.scala
@@ -0,0 +1,67 @@
+package com.tsmms.hackathon.choices.miniwicket
+
+import javax.servlet.http.HttpServletRequest
+
+import scala.xml.{Elem, Node, NodeSeq, Text}
+
+/**
+ * @author <a href="http://www.stoerr.net/">Hans-Peter Stoerr</a>
+ * @since 03.03.2015
+ */
+object MiniWicketProcessor {
+
+  val wicketNS = "http://wicket.apache.org"
+
+  /** contains a Map[String, NodeSeq] wicket:id to content */
+  private val requestAttributeWicketAttributes = "requestAttributeWicketIdToValue"
+
+  private type Processor = (Node) => NodeSeq
+
+  private def processorMap(implicit request: HttpServletRequest): Map[String, Processor] =
+    Option(request.getAttribute(requestAttributeWicketAttributes).asInstanceOf[Map[String, Processor]]).getOrElse(
+      processorMap = Map())
+
+  private def processorMap_=(map: Map[String, Processor])(implicit request: HttpServletRequest) = {
+    request.setAttribute(requestAttributeWicketAttributes, map)
+    map
+  }
+
+  def addField(id: String, value: NodeSeq)(implicit request: HttpServletRequest): Unit = {
+    val fieldProcessor: Processor = (node: Node) => node match {
+      case elem@Elem(prefix, label, attribs, scope, children@_*) =>
+        Elem(prefix, label, attribs, scope, false, value: _*)
+    }
+    processorMap = processorMap + (id -> fieldProcessor)
+  }
+
+  def addField(id: String, value: String)(implicit request: HttpServletRequest): Unit = addField(id, new Text(value))
+
+  def addRepeater(id: String, elements: Seq[() => Unit])(implicit request: HttpServletRequest): Unit = {
+    val repeaterProcessor: Processor = node => elements flatMap (element => {
+      val oldProcessorMap = processorMap
+      element() // changes processorMap temporarily
+      val Elem(prefix, label, attribs, scope, children@_*) = node
+      val processedChildren = children.flatMap(wicketyTransformer(_))
+      val result = Elem(prefix, label, attribs, scope, false, processedChildren: _*)
+      processorMap = oldProcessorMap
+      result
+    })
+    processorMap = processorMap + (id -> repeaterProcessor)
+  }
+
+  private def getWicketIdAction(id: String, request: HttpServletRequest): Processor = processorMap(request)(id)
+
+  def wicketyTransformer(xml: NodeSeq)(implicit request: HttpServletRequest): NodeSeq =
+    xml flatMap (node => node match {
+      case elem: Elem if elem.attribute(wicketNS, "id").isDefined =>
+        val id = node.attribute(wicketNS, "id").get(0).text
+        val action: Processor = getWicketIdAction(id, request)
+        action(elem)
+      case elem@Elem(prefix, label, attribs, scope, children@_*) if elem.attribute(wicketNS, "remove").isDefined =>
+        Text("")
+      case Elem(prefix, label, attribs, scope, children@_*) =>
+        Elem(prefix, label, attribs, scope, false, wicketyTransformer(children): _*)
+      case other => other
+    })
+
+}
---
+++ src/main/scala/com/tsmms/hackathon/choices/miniwicket/MiniWicketServletFilter.scala
@@ -0,0 +1,70 @@
+package com.tsmms.hackathon.choices.miniwicket
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintWriter}
+import java.util.logging.Logger
+import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse, HttpServletResponseWrapper}
+
+import scala.collection.mutable
+import scala.xml._
+
+/**
+ * A servletfilter that simulates some behaviour of apache wicket. Just done since it is much easier use for now.
+ * @author <a href="http://www.stoerr.net/">Hans-Peter Stoerr</a>
+ * @since 02.03.2015
+ */
+class MiniWicketServletFilter extends Filter {
+
+  val logger = Logger.getLogger(getClass.toString)
+
+  var ignoredPaths: Array[String] = Array()
+
+  override def init(filterConfig: FilterConfig): Unit = {
+    ignoredPaths = filterConfig.getInitParameter("ignorePaths").split(",")
+  }
+
+  override def destroy(): Unit = {}
+
+  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
+    val httpRequest = request.asInstanceOf[HttpServletRequest]
+    if (ignoredPaths.find(path => (httpRequest.getRequestURI).contains(path)).isDefined)
+      chain.doFilter(request, response)
+    else {
+      val responseWrapper = new OutputCapturingServletResponseWrapper(response.asInstanceOf[HttpServletResponse])
+      try {
+        chain.doFilter(request, responseWrapper)
+      } finally {
+        responseWrapper.transformOutput(xhtmlTransformer(httpRequest))
+      }
+    }
+  }
+
+  def xhtmlTransformer(request: HttpServletRequest)(in: Array[Byte], out: PrintWriter): Unit = {
+    if (in.length > 0) {
+      val xml = XML.load(new ByteArrayInputStream(in))
+      MiniWicketProcessor.wicketyTransformer(xml)(request) foreach (XML.write(out, _, "UTF-8", false, null))
+    }
+  }
+
+}
+
+class OutputCapturingServletResponseWrapper(response: HttpServletResponse) extends HttpServletResponseWrapper(response) {
+
+  val capturedOutput = new ByteArrayOutputStream()
+
+  /** Seems to be unused. */
+  override def getWriter: PrintWriter = ???
+
+  override def getOutputStream: ServletOutputStream = new ServletOutputStream {
+    override def write(b: Int): Unit = capturedOutput.write(b)
+  }
+
+  def transformOutput(transformer: (Array[Byte], PrintWriter) => Unit): Unit = {
+    val writer = super.getWriter
+    transformer(capturedOutput.toByteArray, writer)
+    writer.close()
+    flushBuffer()
+    capturedOutput.reset()
+  }
+
+}
